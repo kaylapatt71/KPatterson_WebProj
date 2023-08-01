512219bc40a758e49c79aaf2979ef6cdcec150ae
Kayla Patterson (1000112643) <PATTKI1@student.op.ac.nz>
2023-08-02 09:30:26 +1200

    fixed spelling/formatting errors

--- src/lib/NAV.svelte
+++ src/lib/NAV.svelte
@@ -1,8 +1,8 @@
 <nav>
     
-    <a href="https://www.https://online.op.ac.nz/"> OP Student hub </a>
-    <a href="https://www.https://online.op.ac.nz/"> OP Student hub </a>
-    <a href="https://www.https://online.op.ac.nz/"> OP Student hub </a>
-    <a href="https://www.https://online.op.ac.nz/"> OP Student hub </a>
+    <a href="https://www.online.op.ac.nz/"> OP Student hub </a>
+    <a href="https://www.online.op.ac.nz/"> OP Student hub </a>
+    <a href="https://www.online.op.ac.nz/"> OP Student hub </a>
+    <a href="https://www.online.op.ac.nz/"> OP Student hub </a>
 
 </nav>
