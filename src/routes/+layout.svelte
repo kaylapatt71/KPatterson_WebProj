ed5ced4a7ffbd868114545c81a7d5481eb73eea0
Kayla Patterson (1000112643) <PATTKI1@student.op.ac.nz>
2023-08-04 10:41:51 +1200

    added new files etc

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script> 
+    import Header from '$lib/HEADER.svelte';
+    import Footer from '$lib/FOOTER.svelte'; 
+</script>
+
+<Header />
+
+<slot /> <!--page content-->
+
+<Footer />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,4 @@
-<script> 
-    import Header from '$lib/HEADER.svelte';
-    import Footer from '$lib/FOOTER.svelte'; 
-</script>
 
-<Header/>
 
 <h1> Welcome to my site!</h1>
 
-<Footer/> 
