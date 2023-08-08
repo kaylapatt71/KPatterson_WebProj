a188d18c5266b719d2060e96ba58b080305a0caa
Kayla Patterson (1000112643) <PATTKI1@student.op.ac.nz>
2023-08-09 11:12:34 +1200

    stuff

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,15 @@
-<h1>This is my about page!</h1>
+<style>
+    h1{text-align: center;}
+    p {text-align: center;}
+</style>
+
+<body>
+
+<h1>Welcome to The Rolling Pin Bakery.</h1>
+<h1>A family-owned and operated bakery where every day begins with a sprinkle of love and a dash of passion</h1>
+<p>Nestled in the heart of Dunedin, our bakery has been serving up delightful treats for generations. As a family, we believe that baking is not just an art, but a way to bring joy and comfort to our community.</p>
+<p>Our journey started with Ann Goomes, who had a vision of creating a warm and inviting space where families could come together to enjoy freshly baked goodies. Today, that vision lives on as we continue to craft each confection with the same dedication and care that started it all.</p>
+<p>At The Rolling Pin Bakery, we take pride in using only the finest, locally sourced ingredients. Our recipes are cherished family secrets, passed down from one generation to the next. From delectable pastries and cakes to mouthwatering cookies and bread, every bite is a testament to our commitment to quality.</p>
+
+</body>
+
