32650a0f991d240742dbc4b5f5a93178a75b320c
Kayla Patterson (1000112643) <PATTKI1@student.op.ac.nz>
2023-08-16 09:40:59 +1200

    updating

--- src/lib/NAV.svelte
+++ src/lib/NAV.svelte
@@ -1,8 +1,10 @@
-<nav>
-    <a style="text-decoration:none" href="/"> Home </a>
-    <a style="text-decoration:none" href="/about"> Our story </a>
-    <a style="text-decoration:none" href="/customOrders"> Custom orders </a>
-    <a style="text-decoration:none" href="/productGallery"> Product gallery </a>
-    <a style="text-decoration:none" href="/contact"> Contact us </a>
-</nav>
+<div id="nav">
+    <nav>
+        <a style="text-decoration:none" href="/"> Home </a>
+        <a style="text-decoration:none" href="/about"> Our story </a>
+        <a style="text-decoration:none" href="/customOrders"> Custom orders </a>
+        <a style="text-decoration:none" href="/productGallery"> Product gallery </a>
+        <a style="text-decoration:none" href="/contact"> Contact us </a>
+    </nav>
+</div>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,16 @@
 
 
-<h1> Welcome to my site!</h1>
+<h1> Welcome to The Rolling Pin Bakery, a family-owned and operated bakery where every day begins with a sprinkle of love and a dash of passion </h1>
+
+<div class="row">
+    <div class="column">
+        <img src="../img/flour1.jpg" alt="flour" style="width:100%">
+    </div>
+    <div class="column">
+        <img src="../img/flour2.jpg" alt="some more flour" style="width:100%">
+    </div> 
+    <div class="column">
+        <img src="../img/flour.jpg" alt="more flour" style="width:100%">
+    </div>
+</div>
 
