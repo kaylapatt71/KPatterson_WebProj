23690b6e41ea5754b57bd0ae579c33aa65022f19
Kayla Patterson (1000112643) <PATTKI1@student.op.ac.nz>
2023-08-30 09:39:25 +1200

    made flexbox nav

--- src/lib/NAV.svelte
+++ src/lib/NAV.svelte
@@ -1,10 +1,18 @@
-<div id="nav">
-    <nav>
-        <a style="text-decoration:none" href="/"> Home </a>
-        <a style="text-decoration:none" href="/about"> Our story </a>
-        <a style="text-decoration:none" href="/customOrders"> Custom orders </a>
-        <a style="text-decoration:none" href="/productGallery"> Product gallery </a>
-        <a style="text-decoration:none" href="/contact"> Contact us </a>
-    </nav>
-</div>
+<body>
+    <!-- Navbar -->
+    <header>
+        <h1 id="nav-title">The Rolling Pin</h1>
+        <nav>
+            <ul>
+                <a style="text-decoration:none" href="/"> Home </a>
+                <a style="text-decoration:none" href="/about"> Our story </a>
+                <a style="text-decoration:none" href="/customOrders"> Custom orders </a>
+                <a style="text-decoration:none" href="/productGallery"> Product gallery </a>
+                <a style="text-decoration:none" href="/contact"> Contact us </a>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,5 @@
 
 <h1> Welcome to The Rolling Pin Bakery!</h1>
 
-<div class="row">
-    <div class="column">
-        <img src="../img/flour1.jpg" alt="flour" style="width:100%">
-    </div>
-    <div class="column">
-        <img src="../img/flour2.jpg" alt="some more flour" style="width:100%">
-    </div> 
-    <div class="column">
-        <img src="../img/flour.jpg" alt="more flour" style="width:100%">
-    </div>
-</div>
+
 
