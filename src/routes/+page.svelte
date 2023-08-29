05e385951a948f2d2b2501e23ddd4bc5bba4e5a7
Kayla Patterson (1000112643) <PATTKI1@student.op.ac.nz>
2023-08-30 09:51:07 +1200

    ruined everything because i have no idea what im doing in this class lmao

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,4 +3,14 @@
 <h1> Welcome to The Rolling Pin Bakery!</h1>
 
 
+  <body>
+    <div class="container">
+      <div>
+        <img src='..\static\img\logo.jpeg' alt ="logo">
+      </div>
+      <div class="text">
+        <h1>Text go here</h1>
+      </div>
+    </div>
+  </body>
 
