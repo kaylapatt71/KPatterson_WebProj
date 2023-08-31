1ead808b0f3054e7ff3f505afa18eb72d7d4f132
Kayla Patterson (1000112643) <PATTKI1@student.op.ac.nz>
2023-09-01 11:29:47 +1200

    started about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,11 @@
       </div>
       <div class="text">
         <p>Welcome to The Rolling Pin Bakery, a family-owned and operated bakery
-          where every day begins with a sprinkle of love and a dash of passion</p>
+          where every day begins with a sprinkle of love and a dash of passion.
+          At The Rolling Pin Bakery, we invite you to savour the simple joys in life. 
+          Whether you're celebrating a special occasion or just need a moment of indulgence, 
+          we're here to make every moment extraordinary. 
+          Thank you for being a part of our family story.</p>
       </div>
     </div>
 
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,8 +1,68 @@
-<style>
-   
-</style>
+<div class="container">
+    <div class="img">
+      <img src='\img\lemonCupcakes.jpg' alt ="Lemon cupcakes">
+    </div>
+    <div class="text">
+      <p>As a family, we believe that baking
+        is not just an art, but a way to bring joy and comfort to our community.
+        
+        Our journey started with Ann Goomes, who had a vision of creating a warm and inviting space where 
+        families could come together to enjoy freshly baked goodies. Today, that vision 
+        lives on as we continue to craft each confection with the same dedication and care that started it all.</p>
+    </div>
+  </div>
 
-<body>
+  <div class="container"> 
+    <div class="text">
+        <p>At The Rolling Pin Bakery, we take pride in using only the finest, locally sourced ingredients.
+             Our recipes are cherished family secrets, passed down from one generation to the next. 
+             From delectable pastries and cakes to mouthwatering cookies and bread, every bite is a testament to our commitment to quality.
+        </p>
+    </div>
+    <div class="img">
+        <img src='\img\hempCookies.jpg' alt="hemp cookies">
+    </div>
+  </div>
 
-</body>
+  <div>
+    <h2> Our Values</h2>
+
+    <ul>
+        <li>
+            1.	Family-Centred: <br> Our bakery is built on family values, 
+            and we extend that warmth to our customers. 
+            When you step into our bakery, you become a part of our family, 
+            and we treat you with the same love and care we would offer to our own.</li>
+        <li>
+            2.	Quality and Freshness: <br> We believe that great ingredients are the foundation of exceptional baked goods. 
+            That's why we handpick the best local produce and use time-honoured recipes 
+            to ensure that every item is a masterpiece of taste and texture.
+        </li>
+        <li>
+            3.	Community Connection: <br> We are more than just a bakery; 
+            we are an integral part of the community. We support local events, organizations, and initiatives, 
+            aiming to give back and foster a sense of togetherness.
+        </li>
+        <li>
+            4.	Craftsmanship and Tradition: <br> Baking is an art form, 
+            and we take pride in our craftsmanship. With a blend of traditional techniques and innovative approaches, 
+            we honour the past while embracing the future of baking.
+        </li>
+        <li>
+            5.	Customer Delight: <br> Your satisfaction is our ultimate reward. 
+            We go the extra mile to make your experience at our bakery memorable, 
+            whether it's a personalized order or a warm smile to brighten your day.
+        </li>
+        <li>
+            6.	Sustainability: <br> We believe in preserving the planet for future generations. 
+            That's why we are committed to minimizing our environmental impact, 
+            using eco-friendly packaging, and supporting sustainable practices.
+        </li>
+        <li>
+            7.	Innovation and Creativity: <br> While we cherish tradition, 
+            we are also open to new ideas and Flavors. 
+            Our bakers continuously experiment and create exciting new treats to surprise and delight our customers.
+        </li>
+    </ul>
+  </div>
 
