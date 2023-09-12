b807f3072cdf3a65073d1cf705501b5435b20281
Kayla Patterson (1000112643) <PATTKI1@student.op.ac.nz>
2023-09-13 09:03:01 +1200

    fomat gallery

--- src/routes/productGallery/+page.svelte
+++ src/routes/productGallery/+page.svelte
@@ -1,37 +1,31 @@
 <link rel="stylesheet" href="css\gallery.css">
 <div class="row">
     <div class="column">
-      <img src='/img/breadSlice.jpg'  alt = 'bread slices'>
-      <img src='/img/brownies.jpg'  alt = 'brownies'>
       <img src='/img/chocCakeSlice.jpg'  alt = 'chocolate cake slice'>
+      <img src='/img/breadSlice.jpg'  alt = 'bread slices'>
+      <img src='/img/fruitTart.jpg'  alt = 'fruit tarts'>
       <img src='/img/cookieSheet.jpg'  alt = 'cookie sheet'>
       <img src='/img/cookieStack.jpg'  alt = 'cookie stacks'>
-      <img src='/img/fruitTart.jpg'  alt = 'fruit tarts'>
-      <img src='/img/hempCookies.jpg'  alt = 'hemp cookies'>
     </div>
     <div class="column">
-      <img src='/img/lemonCupcakes.jpg'  alt = 'lemon cupcakes'>
-      <img src='/img/muffinsAndCookies.jpg'  alt = 'muffins and cookies'>
-      <img src='/img/pastries.jpg'  alt = 'pastries'>
       <img src='/img/patries.jpg'  alt = 'pastries'>
+      <img src='/img/sausageRolls.jpg'  alt ='sausage rolls'>
+      <img src='/img/carrotCake.jpg' alt = ' carrot cake'>
       <img src='/img/pinkBearCake.jpg'  alt ='pink cake'>
       <img src='/img/rusticWeddingCake.jpg'  alt = 'rustic cake'>
     </div>
     <div class="column">
-      <img src='/img/sausageRolls.jpg'  alt ='sausage rolls'>
       <img src='/img/scrolls.jpg'  alt = 'scrolls'>
       <img src='/img/cakePops.jpg' alt = 'cake pops'>
       <img src='/img/cheeseCake.jpg' alt = 'cheesecake'>
-      <img src='/img/carrotCake.jpg' alt = ' carrot cake'>
-      <img src='/img/applePie.jpg' alt = 'apple pie'>
-      <img src='/img/beeCake.jpg' alt = 'bee cake'>
+      <img src='/img/vanillaCakeSlice.jpg' alt = 'vanilla cake slice'>
+      <img src='/img/hempCookies.jpg'  alt = 'hemp cookies'>
     </div>
     <div class="column">
-      <img src='/img/breadRolls.jpg' alt = 'bread rolls'>
+      <img src='/img/muffinsAndCookies.jpg'  alt = 'muffins and cookies'>
       <img src='/img/macarons.jpg' alt = 'macarons'>
       <img src='/img/merengue.jpg' alt = 'merengue'>
-      <img src='/img/weddingcake.jpg' alt = 'wedding cake'>
-      <img src='/img/vanillaCakeSlice.jpg' alt = 'vanilla cake slice'>
+      <img src='/img/weddingCake.jpg' alt = 'wedding cake'>
       <img src='/img/redCupcake.jpg' alt = 'red cupcake'>
     </div>
   </div>
