16718d8080489254214c80fb495e493a760829c3
Kayla Patterson (1000112643) <PATTKI1@student.op.ac.nz>
2023-11-07 10:07:08 +1300

    added shielded site to page

--- src/lib/FOOTER.svelte
+++ src/lib/FOOTER.svelte
@@ -1,5 +1,19 @@
 <footer>
   <div class="foot">
+    <script src="https://staticcdn.co.nz/embed/embed.js"></script>
+        <script>
+          (function () {
+            window.onload = function () {
+              var frameName = new ds07o6pcmkorn({
+                right: 300,
+                top: 0,
+                iconSize: "small",
+                modalID: "modal",
+              });
+              frameName.init();
+            };
+          })();
+        </script>
     <div class="footleft">
       <div class="exploreheader" />
       <ul class="explorelist17">
