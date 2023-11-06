1eca131c76831b94e7d45ba269cc29f8a2449440
Kayla Patterson (1000112643) <PATTKI1@student.op.ac.nz>
2023-11-07 11:55:45 +1300

    formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,28 @@
   <div class="img">
     <img src="/img/transpLogo.png" alt="transparent logo" />
   </div>
+
   <div class="text">
     <p>
-      Welcome to The Rolling Pin Bakery, a family-owned and operated bakery
-      where every day begins with a sprinkle of love and a dash of passion. At
-      The Rolling Pin Bakery, we invite you to savour the simple joys in life.
-      Whether you're celebrating a special occasion or just need a moment of
-      indulgence, we're here to make every moment extraordinary. Thank you for
-      being a part of our family story.
+      Welcome to The Rolling Pin,
+      A family-owned and operated bakery where every day begins with a sprinkle
+      of love and a dash of passion. At The Rolling Pin Bakery, we invite you to
+      savour the simple joys in life. Whether you're celebrating a special
+      occasion or just need a moment of indulgence, we're here to make every
+      moment extraordinary. Thank you for being a part of our family story.
     </p>
   </div>
 </div>
+
+<style>
+  p {
+    font-size: 1.25rem;
+    color: var(--mainText);
+  }
+
+  h2 {
+    display: flex;
+    color: var(--mainText);
+    font-size: 2rem;
+  }
+</style>
